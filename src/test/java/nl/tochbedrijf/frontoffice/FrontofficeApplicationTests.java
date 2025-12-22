76f9fc37df1f8a6389b3272e4de93d71bf2c2016
Steven Speek <slspeek@gmail.com>
2025-12-22 16:09:47 +0100

    tests use bookRepository where possible

--- src/test/java/nl/tochbedrijf/frontoffice/FrontofficeApplicationTests.java
+++ src/test/java/nl/tochbedrijf/frontoffice/FrontofficeApplicationTests.java
@@ -2,7 +2,9 @@ package nl.tochbedrijf.frontoffice;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import nl.tochbedrijf.frontoffice.entities.Book;
 import nl.tochbedrijf.frontoffice.repositories.BookRepository;
+import org.hibernate.AssertionFailure;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -27,8 +33,11 @@ class FrontofficeApplicationTests {
     @Autowired
     private BookRepository bookRepository;
 
+    public final Book animalFarm = new Book(null, "Animal farm","George Orwell", UUID.randomUUID().toString());
+
     @BeforeEach
     public void deleteAllBeforeTests() throws Exception {
+        System.out.println("Before each");
         bookRepository.deleteAll();
     }
 
@@ -38,21 +47,52 @@ class FrontofficeApplicationTests {
 
     @Test
     public void shouldReturnNoBooks() throws Exception {
-        mockMvc.perform(get("/api/book")).andDo(print()).andExpect(status().isOk())
+        mockMvc.perform(get("/api/book")).andExpect(status().isOk())
                 .andExpect(content().string("[]"));
     }
 
     @Test
     public void shouldDeleteEntity() throws Exception {
+        Book saved = bookRepository.save(animalFarm);
+        Long id = saved.getId();
 
-       mockMvc.perform(post("/api/book").header("Content-Type", "application/json").content(
-                "{ \"author\": \"George Orwell\", \"title\":\"Animal farm\"}")).andDo(print()).andExpect(
-                status().isCreated());
-
-        mockMvc.perform(delete("/api/book/1")).andDo(print()).andExpect(
+        mockMvc.perform(delete(String.format("/api/book/%d", id))).andExpect(
                 status().isNoContent());
 
-        mockMvc.perform(get("/api/book")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string("[]"));
+        List<Book> all = bookRepository.findAll();
+        assert all.isEmpty();
     }
+
+    @Test
+    public void shouldInsertEntity() throws Exception {
+        mockMvc.perform(post("/api/book").header("Content-Type", "application/json").content(
+                "{ \"author\": \"George Orwell\", \"title\":\"Animal farm\"}")).andExpect(
+                status().isCreated());
+
+        List<Book> booksFromDatabase = bookRepository.findAll();
+        if (! booksFromDatabase.isEmpty() ){
+            assert booksFromDatabase.get(0).getAuthor().equals("George Orwell");
+            assert booksFromDatabase.get(0).getTitle().equals("Animal farm");
+
+        } else {
+            throw new AssertionFailure("Expected a record, but none was found");
+        }
+    }
+
+    @Test
+    public void shouldFailInsertEntity() throws Exception {
+        mockMvc.perform(post("/api/book").header("Content-Type", "application/json").content(
+                "{ \"AUTHORXXX\": \"George Orwell\", \"title\":\"Animal farm\"}")).andExpect(
+                status().isCreated());
+
+        List<Book> booksFromDatabase = bookRepository.findAll();
+        if (! booksFromDatabase.isEmpty() ){
+            assert booksFromDatabase.get(0).getAuthor()==null;
+            assert booksFromDatabase.get(0).getTitle().equals("Animal farm");
+
+        } else {
+            throw new AssertionFailure("Expected a record, but none was found");
+        }
+    }
+
 }
