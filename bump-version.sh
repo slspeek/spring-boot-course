e2124d9162e35483ee5a6eccf23e73b87effc4cd
Steven Speek <slspeek@gmail.com>
2026-01-06 14:49:21 +0100

    Bump version to 0.11.0

--- bump-version.sh
+++ bump-version.sh
@@ -2,7 +2,7 @@
 set -e
 
 # create-tag.sh: https://gist.github.com/slspeek/400f682d448e20782d768130f6229f82
-NEW_VERSION=$(create-tag.sh -d)
+NEW_VERSION=$(create-tag.sh -d|sed -e 's/v//')
 mvn versions:set -DnewVersion="$NEW_VERSION"
 git add pom.xml
 git commit -m "Bump version to $NEW_VERSION"
